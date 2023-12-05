b3258dd58eb0fdcde7a64c0dab3f49b782b51d2a
Qossai <52665367+Qossai@users.noreply.github.com>
2023-12-06 00:20:43 +0200

    version 1.6

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 class VIXAnalysisApp:
     def __init__(self):
-        # Update symbols without caret (^) prefix and remove "SVIX"
-        self.symbols = ["VIX9D", "VIX", "VIX3M", "VIX6M"]
+        # Update symbols with caret (^) prefix
+        self.symbols = ["^VIX9D", "^VIX", "^VIX3M", "^VIX6M"]
 
     def fetch_data(self):
         prices = {}
@@ -21,32 +21,32 @@ class VIXAnalysisApp:
 
     def analyze_data(self, prices):
         # Ensure all keys are present in the prices dictionary
-        required_keys = ["VIX9D", "VIX", "VIX3M", "VIX6M"]
+        required_keys = ["^VIX9D", "^VIX", "^VIX3M", "^VIX6M"]
         if not all(key in prices for key in required_keys):
             return "Data Incomplete"
 
-        vix9d = prices.get("VIX9D", float('inf'))
-        vix = prices.get("VIX", float('inf'))
-        vix3m = prices.get("VIX3M", float('inf'))
-        vix6m = prices.get("VIX6M", float('inf'))
+        vix9d = prices.get("^VIX9D", float('inf'))
+        vix = prices.get("^VIX", float('inf'))
+        vix3m = prices.get("^VIX3M", float('inf'))
+        vix6m = prices.get("^VIX6M", float('inf'))
 
         if vix9d < vix < vix3m < vix6m:
-            return "Green: Everything should be OK"
+            return "Green"
         elif vix9d > vix:
-            return "Yellow: It's getting risky"
+            return "Yellow"
         elif vix > vix3m:
             return "Red"
         else:
             return "Neutral"
 
     def run_streamlit_app(self):
-        st.title("VIX Analysis")  # Revert to the original title
+        st.title("Al-Ishara")  # Change page title to "Al-Ishara" and centralize it
 
         prices = self.fetch_data()
         color = self.analyze_data(prices)
 
         # Define background color based on the analysis result
-        background_color = {"Green: Everything should be OK": "green", "Yellow: It's getting risky": "yellow", "Red": "red", "Neutral": "gray"}
+        background_color = {"Green": "green", "Yellow": "yellow", "Red": "red"}
 
         # Define text style for the colored result
         text_style = "font-size: 24px; font-weight: bold; color: black;"
@@ -55,15 +55,16 @@ class VIXAnalysisApp:
         if color in background_color:
             st.markdown(f"<div style='background-color: {background_color[color]}; padding: 10px; border-radius: 5px;'><p style='{text_style}'>{color}</p></div>", unsafe_allow_html=True)
         else:
-            st.write(color)
+            st.write(f"Condition Result: {color}")
 
         # Create a nice table for prices without indexes
-        price_table = [(key, prices[key]) for key in prices if key != "SVIX"]  # Remove "SVIX" from visualization
-        st.sidebar.table(price_table)  # Display the table in the left sidebar
+        st.write("Prices:")
+        price_table = [(key[1:], prices[key]) for key in prices if key != "^SVIX"]  # Remove "^SVIX" from visualization and "^" from symbol names
+        st.table(price_table)
 
         # Visualization
-        fig, ax = plt.subplots(figsize=(10, 6))  # Adjust the figure size
-        ax.bar(prices.keys(), [prices[key] for key in prices if key != "SVIX"])
+        fig, ax = plt.subplots()
+        ax.bar([key[1:] for key in prices if key != "^SVIX"], [prices[key] for key in prices if key != "^SVIX"])  # Remove "^" from symbol names
         ax.set_ylabel('Prices')
         ax.set_title('VIX Prices Visualization')
 
